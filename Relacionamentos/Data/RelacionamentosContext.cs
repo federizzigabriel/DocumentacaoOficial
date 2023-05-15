474a5aa3dac2a2647251879df27637dc3e3102f3
federizzigabriel <74614124+federizzigabriel@users.noreply.github.com>
2023-05-15 11:34:14 -0300

    Adicionar arquivos de projeto.

---
+++ Relacionamentos/Data/RelacionamentosContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Relacionamentos.Models;
+
+namespace Relacionamentos.Data
+{
+    public class RelacionamentosContext : DbContext
+    {
+        public RelacionamentosContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Blog>? Blogs { get; set; }
+        public DbSet<Post>? Posts { get; set; }
+    }
+}
+
